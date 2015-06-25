daa1339602c43d05ed906933dc60ed6a8357e148
Dan Schultz <dan@formula23.com>
2015-06-24 22:10:43 -0700

    config update

--- config/squeegy.php
+++ config/squeegy.php
@@ -2,11 +2,12 @@
 
 return [
     "operating_hours" => [
-        "open" => 9,
-        "close" => 22,
+        "open" => 0,
+        "close" => 23,
         "max_lead_time" => 120,
     ],
     "sms_verification" => "6538",
+    "cancellation_fee" => 1000,
     'emails' => [
         'welcome' => [
             'subject' => 'Welcome to Squeegy!',
