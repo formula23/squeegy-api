510b6ddb671b5f5fd67614f7faadcd2048f91c9c
Dan Schultz <dan@formula23.com>
2015-06-27 02:39:21 -0700

    fix

--- app/Events/OrderCancelledByWorker.php
+++ app/Events/OrderCancelledByWorker.php
@@ -1,21 +1,23 @@
 <?php namespace App\Events;
 
 use App\Events\Event;
-
+use App\Order;
 use Illuminate\Queue\SerializesModels;
 
 class OrderCancelledByWorker extends Event {
 
 	use SerializesModels;
 
-	/**
-	 * Create a new event instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
+    public $order;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Order $order
+     */
+	public function __construct(Order $order)
 	{
-		//
+        $this->order = $order;
 	}
 
 }
