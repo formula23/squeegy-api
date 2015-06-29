58bd045822888364a80925eb8ed63dcb01d965f4
Dan Schultz <dan@formula23.com>
2015-06-28 19:44:27 -0700

    update

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -26,6 +26,7 @@ abstract class Request extends FormRequest {
 
     public function response(array $errors)
     {
-        return new JsonResponse(['error' => ['http_code'=>400, 'message' => $errors]], 400);
+        throw new \Exception("ERRRORRRR!!");
+//        return new JsonResponse(['error' => ['http_code'=>400, 'message' => $errors]], 400);
     }
 }
