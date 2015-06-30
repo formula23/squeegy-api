375857476c9318b1657d7a069c360b4d501d365c
Dan Schultz <dan@formula23.com>
2015-06-30 14:23:18 -0700

    only charge cancel fee if eta is less than 30 mins. otherwise, refund full authorization

--- app/Handlers/Events/ChargeCancelFee.php
+++ app/Handlers/Events/ChargeCancelFee.php
@@ -1,6 +1,7 @@
 <?php namespace App\Handlers\Events;
 
 use App\Events\OrderCancelled;
+use App\Squeegy\Orders;
 use App\Squeegy\Payments;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldBeQueued;
@@ -30,7 +31,11 @@ class ChargeCancelFee {
 
         $payments = new Payments($event->order->customer->stripe_customer_id);
 
-        $charge = $payments->cancel($event->order->stripe_charge_id, $cancel_fee);
+        if(Orders::getCurrentEta($event->order) < 30) {
+            $charge = $payments->cancel($event->order->stripe_charge_id, $cancel_fee);
+        } else {
+            $charge = $payments->refund($event->order->stripe_charge_id);
+        }
 
         $event->order->charged = $cancel_fee;
         $event->order->stripe_charge_id = $charge->id;
