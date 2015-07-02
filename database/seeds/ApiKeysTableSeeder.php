759010f1b84f519f771b1564778f3d67bfc06568
Dan Schultz <dan@formula23.com>
2015-07-02 14:16:00 -0700

    api key seeder

--- database/seeds/ApiKeysTableSeeder.php
+++ database/seeds/ApiKeysTableSeeder.php
@@ -17,23 +17,13 @@ class ApiKeysTableSeeder extends Seeder
         $apiLog->truncate();
 
         /**
-         * Local/Dev/Stage
+         * Squeegy consumer app
          */
         $apiKey->key = "bf9c37004d3155881b48fe54ec68024809a8e723";
         $apiKey->user_id = 0;
         $apiKey->level = 10;
         $apiKey->ignore_limits = 1;
         $apiKey->save();
-
-        /**
-         * Production
-         */
-        $apiKey->key = "7b1731fc7d961cd201ff9d682ba876d59f46c5b4";
-        $apiKey->user_id = 0;
-        $apiKey->level = 10;
-        $apiKey->ignore_limits = 1;
-        $apiKey->save();
-
 
         DB::statement('SET FOREIGN_KEY_CHECKS = 1'); // enable foreign key constraints
     }
