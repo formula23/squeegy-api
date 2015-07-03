81e39323c9942d18af957ccf7456b9cf9c0b6a2f
Dan Schultz <dan@formula23.com>
2015-07-03 12:48:09 -0700

    update promo for test

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -231,7 +231,7 @@ class OrdersController extends Controller {
     {
         if (isset($request_data['promo_code'])) { //calculate promo
             if ($request_data['promo_code'] == "1234" && !$order->discount) {
-                $request_data['discount'] = 500;
+                $request_data['discount'] = ($order->service_id==1 ? 2300 : 3300);
                 return $request_data;
             }
             return $request_data;
