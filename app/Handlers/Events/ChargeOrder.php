c2d16b03c3bb3fb9e0b5156718791212bb5dbd69
Dan Schultz <dan@formula23.com>
2015-07-07 15:20:19 -0700

    update charge order to catch an exceptions.

--- app/Handlers/Events/ChargeOrder.php
+++ app/Handlers/Events/ChargeOrder.php
@@ -25,16 +25,20 @@ class ChargeOrder {
 	 */
 	public function handle(OrderDone $event)
 	{
-        $order_amount = $event->order->price - (int)$event->order->discount;
+        try {
+            $order_amount = $event->order->price - (int)$event->order->discount;
 
-        $payments = new Payments($event->order->customer->stripe_customer_id);
+            $payments = new Payments($event->order->customer->stripe_customer_id);
 
-        $charge = $payments->capture($event->order->stripe_charge_id);
+            $charge = $payments->capture($event->order->stripe_charge_id);
 
-        $event->order->charged = $order_amount;
-        $event->order->stripe_charge_id = $charge->id;
-        $event->order->save();
-
+            $event->order->charged = $order_amount;
+            $event->order->stripe_charge_id = $charge->id;
+            $event->order->save();
+            
+        } catch(\Exception $e) {
+            \Bugsnag::notifyException(new \Exception($e->getMessage()));
+        }
 	}
 
 }
