15c788c4dff1ffe78a44c12ed62c9282a7377d10
Dan Schultz <dan@formula23.com>
2015-09-16 10:35:03 -0700

    update push notify

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -11,20 +11,30 @@ class NotifyController extends Controller {
 	public function push(Request $request)
     {
 //        if(!$request->input('user_id')) return;
-        
-        //anonymous users
-        $users = User::where('app_version', '>=', '1.3')
-            ->where('is_active', 1)
-            ->whereNotNull('push_token')
-            ->where('push_token', '!=', '')
-            ->where('email', 'like', '%squeegyapp-tmp.com%')
-            ->get();
 
-//        $users = User::whereIn('id', explode(",",$request->input('user_id')))->get();
+        if($request->input('user_id')) {
+            $users = User::whereIn('id', explode(",",$request->input('user_id')))->get();
+
+        } else {
+            //anonymous users
+//            $users = User::where('app_version', '>=', '1.3')
+//                ->where('is_active', 1)
+//                ->whereNotNull('push_token')
+//                ->where('push_token', '!=', '')
+//                ->where('email', 'like', '%squeegyapp-tmp.com%')
+//                ->get();
+        }
+
+        print "user count:".$users->count()."\n";
+        print "sent message:\n\n";
+        print $request->input('message')."\n\n";
 
         foreach($users as $user) {
             if(empty($user->push_token)) continue;
-            PushNotification::send($user->push_token, $request->input('message'), 0);
+
+            PushNotification::send($user->push_token, $request->input('message'), 1);
+
+            print "sent to: ".$user->push_token."\n";
         }
 
     }
