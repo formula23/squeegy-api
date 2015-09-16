ca6927ce94d7083998ba44184299181480b0fd6b
Dan Schultz <dan@formula23.com>
2015-09-16 10:09:58 -0700

    push notification update

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -10,10 +10,17 @@ class NotifyController extends Controller {
 
 	public function push(Request $request)
     {
-        if(!$request->input('user_id')) return;
+//        if(!$request->input('user_id')) return;
+        
+        //anonymous users
+        $users = User::where('app_version', '>=', '1.3')
+            ->where('is_active', 1)
+            ->whereNotNull('push_token')
+            ->where('push_token', '!=', '')
+            ->where('email', 'like', '%squeegyapp-tmp.com%')
+            ->get();
 
-//        $users = User::where('app_version', '>=', '1.3')->where('is_active', 1)->whereNotNull('push_token')->get();
-        $users = User::whereIn('id', explode(",",$request->input('user_id')))->get();
+//        $users = User::whereIn('id', explode(",",$request->input('user_id')))->get();
 
         foreach($users as $user) {
             if(empty($user->push_token)) continue;
