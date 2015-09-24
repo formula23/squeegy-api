fc6055c0f3bbd243ad550bf42d0d0656161a020c
Dan Schultz <dan@formula23.com>
2015-09-24 13:58:55 -0700

    fix

--- app/Squeegy/Orders.php
+++ app/Squeegy/Orders.php
@@ -151,7 +151,7 @@ class Orders {
         $total_workers = User::workers()->where('users.is_active',1)->get()->count();
 
         $orders_in_q = Order::query();
-        $orders_in_q->whereIn('status', ['confirm','enroute','start'])->where('promo_code', '!=', 'training');
+        $orders_in_q->whereIn('status', ['confirm','enroute','start']);
         $open_orders = $orders_in_q->get();
 
         $available_workers = $total_workers - $open_orders->count();
