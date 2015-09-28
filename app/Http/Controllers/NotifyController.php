af2966f166659aa65dd8bf65b049aac0c3a7b808
Dan Schultz <dan@formula23.com>
2015-09-28 10:21:27 -0700

    push

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -41,6 +41,11 @@ class NotifyController extends Controller {
                 ->where('email', 'like', '%squeegyapp-tmp.com%');
 
 
+            $user_qry = User::where('app_version', '1.4')->where('push_token', '!=', '')
+                ->where('email', 'like', '%squeegyapp-tmp.com%')
+                ->where('created_at', '>=', '2015-09-26');
+
+
 //            $user_qry = User::join('orders', 'users.id', '=', 'orders.user_id')
 //                ->where('app_version', '>=', '1.3')
 //                ->where('users.is_active', 1)
