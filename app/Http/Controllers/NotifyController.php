558813cbf3ee8969968875a5825609ca76f388f6
Dan Schultz <dan@formula23.com>
2015-10-02 13:49:38 -0700

    push

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -39,7 +39,7 @@ class NotifyController extends Controller {
 
             $user_qry = User::where('app_version', '1.4')->where('push_token', '!=', '')
                 ->where('email', 'not like', '%squeegyapp-tmp.com%')
-                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-09-25')
+                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '<', '2015-09-25')
                 ->orderBy('id');
 
 //            $user_qry = User::where('app_version', '1.4')->where('push_token', '!=', '')
