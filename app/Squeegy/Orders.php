39bb57557833702f4268a289fc50dee267deac5b
Dan Schultz <dan@formula23.com>
2015-11-12 12:16:18 -0800

    fix

--- app/Squeegy/Orders.php
+++ app/Squeegy/Orders.php
@@ -296,7 +296,9 @@ class Orders {
                 }
             }
         }
-
+        $msg = print_r($complete_times_by_worker, 1);
+        $msg .= print_r($next_available, 1);
+mail("dan@formula23.com", "eta", $msg);
 //        print_r($complete_times_by_worker);
 //        print_r($next_available);
 //        exit;
