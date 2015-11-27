565b5ae19877844a11818a81d1c0404fca6dfa96
Dan Schultz <dan@formula23.com>
2015-11-27 11:39:26 -0800

    push test code

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -20,19 +20,26 @@ class NotifyController extends Controller {
             $users = User::whereIn('id', explode(",",$request->input('user_id')))->get();
         } else {
 
-//            \DB::connection()->enableQueryLog();
-//
-//            $users = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
-//                ->where('email', 'like', '%squeegyapp-tmp.com%')
-//                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '<=', '2015-11-10')->get();
-//
-//            foreach ($users as $user)
-//            {
-//                print_r($user);
-//            }
-//
-//            $queries = \DB::getQueryLog();
-//            dd($queries);
+            \DB::connection()->enableQueryLog();
+
+            $users = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
+                    ->whereNotIn('id', function($q) {
+                        $q->select('user_id')
+                            ->from('ordes')
+                            ->where('status', 'done')
+                            ->where('confirm_at', '>', '2015-11-26')
+                            ->whereOr(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-11-27');
+                })
+                ->get();
+            $queries = \DB::getQueryLog();
+            print_r($queries);
+
+            dd($users);
+
+
+
+            dd($queries);
+
 
             //anonymous users
 //            $users = User::where('app_version', '>=', '1.3')
@@ -47,11 +54,10 @@ class NotifyController extends Controller {
 //                ->where('created_at', '<', '2015-09-28')
 //                ->orderBy('id');
 //
-            $user_qry = User::select(['id', 'push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
-                ->where('email', 'like', '%squeegyapp-tmp.com%')
-//                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '<=', '2015-11-10')
-                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-11-26')
-                ->orderBy('id');
+//            $user_qry = User::select(['id', 'push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
+//                ->where('email', 'like', '%squeegyapp-tmp.com%')
+//                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-11-26')
+//                ->orderBy('id');
 
 //            $user_qry = User::where('app_version', '1.4')->where('push_token', '!=', '')
 //                ->whereIn('id', [161,260,406,454,521,436,170,531,390,287,463,781,997,898,1025,1067,2288,1080,1039,1153,1174,1178,1127,1202,1177,1301,1289,1423,1306,1306,1489,1500,1576,1549,1518,1622,1679,1750,1615,1389,1034,1060,1856,1800,1893,1284,1507,1109]);
@@ -96,7 +102,7 @@ class NotifyController extends Controller {
 //                ->whereIn('orders.status', ['done'])
 //                ->where('orders.done_at', '<', '2015-09-16')
 //                ->groupBy('users.id');
-            $users = $user_qry->get();
+//            $users = $user_qry->get();
         }
 //dd($users);
         $send_list = array_merge($users->toArray(), $default_users->toArray());
@@ -109,7 +115,7 @@ class NotifyController extends Controller {
 
             if(empty($user['push_token'])) continue;
 
-            PushNotification::send($user['push_token'], $request->input('message'), 1);
+//            PushNotification::send($user['push_token'], $request->input('message'), 1);
 
             print "sent to: id# ".$user['id']." - ".$user['push_token']."\n";
         }
