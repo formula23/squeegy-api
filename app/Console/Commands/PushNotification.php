c97f7fc3c3b647182e9172d8397254434224101c
Dan Schultz <dan@formula23.com>
2015-12-02 14:13:32 -0800

    push cmd

--- app/Console/Commands/PushNotification.php
+++ app/Console/Commands/PushNotification.php
@@ -1,6 +1,8 @@
 <?php namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 
 class PushNotification extends Command {
 
@@ -17,6 +19,9 @@ class PushNotification extends Command {
 	 * @var string
 	 */
 	protected $description = 'Send Push Notification';
+
+    protected $sns_client = null;
+    protected $message = "";
 
 	/**
 	 * Create a new command instance.
@@ -35,13 +40,23 @@ class PushNotification extends Command {
 	 */
 	public function fire()
 	{
+        $type = $this->argument('type');
+        $topic_name = $this->option('topic_name');
+        $this->message = $this->option('message');
+
+        if(!$this->message) {
+            $this->error('Message is required!');
+            return;
+        }
+
+        if($type == "topic" && !$topic_name) {
+            $this->error('Topic name is required to create a topic!');
+            return;
+        }
+
         $default_users = \DB::table('users')->select(['id','push_token'])->where('email', 'dan@formula23.com')->orWhere('email', 'sinisterindustries@yahoo.com')->get();
 
-//        $message = "If you missed black friday. We've extended it, use promo code: BLACKFRIDAY to get 50% off your car wash.";
-//        $message = "Get your car cleaned on cyber monday for 40% off with code CYBER40";
-        $message = "Don't forget, your first car wash is on us. Request now! - Use Promo Code: FREE";
-
-//        $users = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
+        //        $users = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
 //            ->whereNotIn('id', function($q) {
 //                $q->select('user_id')
 //                    ->from('orders')
@@ -53,64 +68,60 @@ class PushNotification extends Command {
 //            ->skip(2000)
 //            ->get();
 
-            $user_qry = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
-                ->where('email', 'like', '%squeegyapp-tmp.com%')
-                ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-12-01')
-                ->orderBy('id');
+        $user_qry = \DB::table('users')->select(['id','push_token'])->where('app_version', '1.4')->where('push_token', '!=', '')
+            ->where('email', 'like', '%squeegyapp-tmp.com%')
+            ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2015-12-01')
+            ->orderBy('id');
 
-            $users = $user_qry->get();
-
-        //create topic
-        $push_client = \App::make('Aws\Sns\SnsClient');
-
-        $resp = $push_client->CreateTopic(['Name' => 'Daily Push']);
-        $topic_arn = $resp->get('TopicArn');
-
-//        print_r($default_users->toArray());
-// dd($users->toArray());
+        $users = $user_qry->get();
         $send_list = array_merge($users, $default_users);
-
         $this->info("user count: ".count($send_list));
 
+        $this->info("publish message: ".$this->message);
 
-        foreach($send_list as $user) {
-            if(empty($user->push_token)) continue;
+        if($this->argument('env') == "test") {
 
-            $push_client->Subscribe([
-                'TopicArn' => $topic_arn,
-                'Protocol' => 'application',
-                'Endpoint' => $user->push_token,
-            ]);
+            foreach($send_list as $user) {
+                $this->_output($user);
+            }
 
-            $this->info('user id subscribed: ' . $user->id);
+        } else {
+
+            $this->sns_client = \App::make('Aws\Sns\SnsClient');
+
+            if($type == "topic") {
+
+                $this->info('Topic created: '.$topic_name);
+
+                $resp = $this->sns_client->CreateTopic(['Name' => $topic_name]);
+
+                $topic_arn = $resp->get('TopicArn');
+                $this->info("TopicArn: ".$topic_arn);
+
+                foreach($send_list as $user) {
+                    if(empty($user->push_token)) continue;
+
+                    $this->sns_client->Subscribe([
+                        'TopicArn' => $topic_arn,
+                        'Protocol' => 'application',
+                        'Endpoint' => $user->push_token,
+                    ]);
+                    $this->_output($user);
+                }
+
+                $this->info('Publish to TopicArn');
+                $this->publish($topic_arn);
+
+            } else {
+
+                foreach ($send_list as $user) {
+                    if (empty($user->push_token)) continue;
+
+                    $this->publish($user->push_token);
+                    $this->_output($user);
+                }
+            }
         }
-
-        $this->info("publish message: ".$message);
-
-        $aps_payload = [
-            'aps' => [
-                'alert' => $message,
-                'sound' => 'default',
-                'badge' => 1
-            ],
-        ];
-
-        $message = json_encode([
-            'default' => $message,
-            env('APNS') => json_encode($aps_payload)
-        ]);
-
-        $push_client->publish([
-            'TargetArn' => $topic_arn,
-            'MessageStructure' => 'json',
-            'Message' => $message,
-        ]);
-
-        $this->info("Delete Topic");
-
-        $push_client->deleteTopic([
-            'TopicArn' => $topic_arn,
-        ]);
 
         $this->info("Done!");
 
@@ -124,7 +135,8 @@ class PushNotification extends Command {
 	protected function getArguments()
 	{
 		return [
-//			['example', InputArgument::REQUIRED, 'An example argument.'],
+			['env', InputArgument::OPTIONAL, 'Run as test or live', 'test'],
+			['type', InputArgument::OPTIONAL, 'Create a topic or send direct message', 'direct'],
 		];
 	}
 
@@ -136,8 +148,38 @@ class PushNotification extends Command {
 	protected function getOptions()
 	{
 		return [
-//			['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+			['message', null, InputOption::VALUE_REQUIRED, 'The message to send.', null],
+			['topic_name', null, InputOption::VALUE_OPTIONAL, 'Topic name.', null],
 		];
 	}
+
+    protected function _output($user)
+    {
+        $this->info('user id: ' . $user->id." -- ".$user->push_token);
+    }
+
+    private function publish($topic_arn)
+    {
+        $aps_payload = [
+            'aps' => [
+                'alert' => $this->message,
+                'sound' => 'default',
+                'badge' => 1
+            ],
+        ];
+
+        $message = json_encode([
+            'default' => $this->message,
+            env('APNS') => json_encode($aps_payload)
+        ]);
+
+        $this->push_client->publish([
+            'TargetArn' => $topic_arn,
+            'MessageStructure' => 'json',
+            'Message' => $message,
+        ]);
+
+        return;
+    }
 
 }
