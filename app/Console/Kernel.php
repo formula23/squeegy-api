363b749df0a52946db24eda1e87494adee2867e6
Dan Schultz <dan@formula23.com>
2015-12-02 16:45:06 -0800

    update db backup to every 6 hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('db:backup')->everyFiveMinutes();
+        $schedule->command('db:backup')->cron('* */6 * * *');
 	}
 
 }
