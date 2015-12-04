cdc16ee75a3ed7043763dfefffcb328d9b69edb7
Dan Schultz <dan@formula23.com>
2015-12-03 17:37:43 -0800

    update cron schedule for db backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-        $schedule->command('db:backup')->cron('* */6 * * *');
+        $schedule->command('db:backup')->cron('* */4 * * *');
 	}
 
 }
