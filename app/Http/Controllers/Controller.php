6f206c69652406b172608b56f611bff804260205
Dan Schultz <dan@formula23.com>
2016-01-06 14:36:40 -0800

    add api key validation back to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -57,7 +57,7 @@ abstract class Controller extends ApiGuardController {
 
             // We should check if key authentication is enabled for this method
             $keyAuthentication = true;
-/*
+
             if (isset($apiMethods[$method]['keyAuthentication']) && $apiMethods[$method]['keyAuthentication'] === false) {
                 $keyAuthentication = false;
             }
@@ -98,7 +98,6 @@ abstract class Controller extends ApiGuardController {
                     }
                 }
             }
-            */
 
             $apiLog = App::make(Config::get('apiguard.apiLogModel', 'Chrisbjr\ApiGuard\Models\ApiLog'));
 
