44e07b517352d30893dcd0b9102fa192e2d42b3d
Dan Schultz <dan@formula23.com>
2016-01-12 14:35:22 -0800

    washer app versions

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -13,13 +13,30 @@ class VersionController extends Controller {
     public function check(Request $request)
     {
         $user_app_version = $request->input('app_version');
+        $user_app_type = $request->input('app_type');
         if($user_app_version) {
-            $version = "APP_VERSION".($request->header('X-Device') == "Android" ? "_ANDROID" : "" );
-            $min_app_version = (float)env($version);
-            if($user_app_version < $min_app_version) {
-                return $this->response->withArray(["status"=>"upgrade"]);
+
+            $android = ($request->header('X-Device') == "Android" ? "_ANDROID" : "" );
+
+            if($user_app_type=="washer") {
+
+                $min_app_version = (float)env("WASHER_APP_VERSION".$android);
+                $install_link = env('WASHER_APP_INSTALL'.$android);
+
+                if($user_app_version < $min_app_version && $install_link) {
+                    return $this->response->withArray(['status'=>'upgrade', 'install_link'=>$install_link]);
+                } else {
+                    return $this->response->withArray(["status"=>"ok"]);
+                }
+
             } else {
-                return $this->response->withArray(["status"=>"ok"]);
+                $min_app_version = (float)env("APP_VERSION".$android);
+
+                if($user_app_version < $min_app_version) {
+                    return $this->response->withArray(["status"=>"upgrade"]);
+                } else {
+                    return $this->response->withArray(["status"=>"ok"]);
+                }
             }
         }
     }
