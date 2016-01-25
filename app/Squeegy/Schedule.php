34c48aa9ca115f58ac1526c4e3181db168c23110
Dan Schultz <dan@formula23.com>
2016-01-24 23:48:02 -0800

    update available schedule format

--- app/Squeegy/Schedule.php
+++ app/Squeegy/Schedule.php
@@ -18,7 +18,7 @@ class Schedule
     public $now;
     public $open_hr;
     public $close_hr;
-    public $available=[];
+//    public $available=[];
     public $lead_hrs=3;
     public $days_out=5;
     public $current_schedule;
@@ -32,31 +32,38 @@ class Schedule
         $this->open_hr = Config::get('squeegy.operating_hours.open');
         $this->close_hr = Config::get('squeegy.operating_hours.close');
 
-//		$current_day = 7;
-//		$this->now = Carbon::create(2016,01,$current_day,9,0,0);
+//		$this->current_day = 8;
+//		$this->now = Carbon::create(2016,01,$this->current_day,0,0,0);
 
     }
 
     public function availability()
     {
+
+        $idx=0;
         for($i=0; $i<=$this->days_out; $i++)
         {
             $this->now->addDay((!$i?0:1));
+
+            if(empty($container)) $container=[];
 
             if($this->now->isSunday()) {
                 $this->days_out+=1;
                 continue;
             }
 
+            $container[$idx] = ['day'=>$this->now->format('l, F d')];
+
             $this->open = $this->open_hr;
             $this->close = $this->close_hr;
+            $windows=[];
 
             for($this->open; $this->open<=$this->close-1; $this->open++) {
 
                 $start = new Carbon($this->now->format("m/d/y $this->open:00"));
 
                 if($this->now->isToday()) {
-//				if($this->now->day == $current_day) {
+//				if($this->now->day == $this->current_day) {
 
                     if($this->now->hour >= $this->close) {
 //						print "after close\n";
@@ -82,11 +89,21 @@ class Schedule
 
                 $is_available = (!empty($this->current_schedule[$key]) && $this->current_schedule[$key]>=3?false:true);
 
-                $this->available[$this->now->format('l, F d')][] = ["window"=>$start->format('g:00a')." - ".$start->addHours(1)->format('g:00a'), "available"=>$is_available];
+                if( ! $is_available) continue;
+
+                //, "available"=>$is_available
+
+                $windows[] = $start->format('g:00a')." - ".$start->addHours(1)->format('g:00a');
+
+            }
+
+            if(count($windows)) {
+                $container[$idx]['time_slots'] = $windows;
+                $idx+=1;
             }
         }
 
-        return $this->available;
+        return $container;
     }
 
     protected function current_schedule()
