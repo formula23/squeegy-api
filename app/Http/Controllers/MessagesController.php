ee31730df6b0e8c2862d9388b532c7db7f8e09c3
Dan Schultz <dan@formula23.com>
2016-01-29 14:00:40 -0800

    create messages controller for various messaging in the app

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,30 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function getIndex()
+	{
+		$data=[
+			'referrer_friend' => [
+				'header' => 'Invite friends. Get free washes.',
+				'body' => 'Send friends a free car wash and you\'ll get one to, worth $15.',
+			],
+			'create_password' => [
+				'header' => 'Create a Password',
+				'body' => 'We\'ve added many new features to this version of Squeegy. To continue, please add a password to your account.',
+			],
+		];
+		return $this->response->withArray($data);
+	}
+
+}
