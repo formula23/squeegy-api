dd83022660cfec13e26eb8afed2a854554f5ad5c
Dan Schultz <dan@formula23.com>
2016-02-08 11:31:59 -0800

    push 2/2

--- app/Console/Commands/PushNotification.php
+++ app/Console/Commands/PushNotification.php
@@ -97,7 +97,7 @@ class PushNotification extends Command {
         //daily anonymous users push
         $users_qry = \DB::table('users')->select(['id','push_token'])->where('push_token', '!=', '')
             ->where('email', 'like', '%squeegyapp-tmp.com%')
-            ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2016-02-01')
+            ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '=', '2016-02-02')
 //            ->where(\DB::raw('DATE_FORMAT(created_at, \'%Y-%m-%d\')'), '<=', '2016-01-26')
             ->orderBy('id');
 
