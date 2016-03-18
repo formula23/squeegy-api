e9fb0d5d640c4b4e4218b25971b43ec5d3ef1579
Dan Schultz <dan@formula23.com>
2016-03-17 22:06:47 -0700

    update email receipt summary

--- resources/views/emails/partials/receipt_details.blade.php
+++ resources/views/emails/partials/receipt_details.blade.php
@@ -1,3 +1,3 @@
 @foreach($order->order_details as $order_detail)
-    <span style="font-size:12px">{{ $order_detail->name }}&nbsp;<span style="float:right">${{ number_format($order_detail->amount/100, 2) }}</span></span><br />
+    <span style="font-size:12px">{{ $order_detail->name }} Wash<span style="float:right">${{ number_format($order_detail->amount/100, 2) }}</span></span><br />
 @endforeach
