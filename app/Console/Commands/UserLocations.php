d5eb7e05161bf2ec4bcb23c9f180f01ff13a96c8
Dan Schultz <dan@formula23.com>
2016-03-30 12:07:12 -0700

    user location console

--- app/Console/Commands/UserLocations.php
+++ app/Console/Commands/UserLocations.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class UserLocations extends Command
 {
@@ -11,14 +13,14 @@ class UserLocations extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'user:locations';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = '';
 
     /**
      * Create a new command instance.
@@ -37,6 +39,22 @@ class UserLocations extends Command
      */
     public function handle()
     {
-        //
+        $locations = DB::select('SELECT orders.location FROM user_segments, orders WHERE user_segments.user_id = orders.user_id AND `segment_id` = 4 AND orders.status = \'done\'');
+
+        $locations = DB::select('SELECT orders.location FROM orders WHERE user_id IN (SELECT users.id FROM orders
+                JOIN users ON orders.user_id = users.id
+                LEFT JOIN user_segments ON users.id = `user_segments`.user_id
+                WHERE `status` = \'done\'
+                GROUP BY orders.user_id HAVING sum(charged)/100 > 300
+                ORDER BY sum(charged) DESC)
+                AND orders.status = \'done\'');
+
+        foreach($locations as $location) {
+            $loc = json_decode($location->location);
+            $this->info("T,".$loc->lat.",".$loc->lon);
+        }
+
+
+
     }
 }
