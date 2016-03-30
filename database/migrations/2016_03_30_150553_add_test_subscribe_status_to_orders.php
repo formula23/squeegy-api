67262b47c7292166621ef30d9f738882c6c0c86f
Dan Schultz <dan@formula23.com>
2016-03-30 15:26:56 -0700

    migration add test and subscribe status'

---
+++ database/migrations/2016_03_30_150553_add_test_subscribe_status_to_orders.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTestSubscribeStatusToOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            \DB::statement("ALTER TABLE `orders` CHANGE `status` `status` ENUM('test', 'request','confirm','receive','schedule','subscribe','assign','enroute','start','done','cancel')");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            \DB::statement("ALTER TABLE `orders` CHANGE `status` `status` ENUM('request','confirm','receive','schedule','assign','enroute','start','done','cancel')");
+        });
+    }
+}
