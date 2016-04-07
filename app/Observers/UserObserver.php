6f432bff20500ab1a4c2ec836513498add56d9c7
Dan Schultz <dan@formula23.com>
2016-04-06 18:29:42 -0700

    update mix panel integration

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danschultz
+ * Date: 4/6/16
+ * Time: 17:40
+ */
+
+namespace App\Observers;
+
+
+use GeneaLabs\LaravelMixpanel\LaravelMixpanel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class UserObserver
+{
+
+    protected $request;
+    protected $mixPanel;
+
+    /**
+     * UserObserver constructor.
+     * @param Request $request
+     * @param LaravelMixpanel $mixpanel
+     */
+    public function __construct(Request $request, LaravelMixpanel $mixpanel)
+    {
+        $this->request = $request;
+        $this->mixPanel = $mixpanel;
+    }
+
+    /**
+     * @param Model $user
+     */
+    public function saved(Model $user)
+    {
+        $firstName = $user->first_name;
+        $lastName = $user->last_name;
+
+        if ($user->name) {
+            $nameParts = explode(' ', $user->name);
+            array_filter($nameParts);
+            $lastName = array_pop($nameParts);
+            $firstName = implode(' ', $nameParts);
+        }
+
+        $data = [
+            '$first_name' => $firstName,
+            '$last_name' => $lastName,
+            '$name' => $user->name,
+            '$email' => $user->email,
+            '$phone' => substr($user->phone, 2),
+            '$created' => ($user->created_at
+                ? $user->created_at->format('Y-m-d\Th:i:s')
+                : null),
+            "Available Credits"=>$user->availableCredit()/100,
+            "Referral Code" =>$user->referral_code,
+            "Segment ID" =>$user->segment->segment_id,
+        ];
+        array_filter($data);
+
+        Log::info('User Observer - Saved...');
+        Log::info($data);
+
+        if (count($data)) {
+            Log::info('User key:'.$user->getKey());
+            $result = $this->mixPanel->people->set($user->getKey(), $data, $this->request->ip());
+            Log::info($result);
+        }
+    }
+
+    /**
+     * @param Model $user
+     */
+//    public function updated(Model $user)
+//    {
+//        $this->mixPanel->identify($user->getKey());
+//        $firstName = $user->first_name;
+//        $lastName = $user->last_name;
+//
+//        if ($user->name) {
+//            $nameParts = explode(' ', $user->name);
+//            array_filter($nameParts);
+//            $lastName = array_pop($nameParts);
+//            $firstName = implode(' ', $nameParts);
+//        }
+//
+//        $data = [
+//            '$first_name' => $firstName,
+//            '$last_name' => $lastName,
+//            '$name' => $user->name,
+//            '$email' => $user->email,
+//            '$created' => ($user->created_at
+//                ? $user->created_at->format('Y-m-d\Th:i:s')
+//                : null),
+//        ];
+//        array_filter($data);
+//
+//        if (count($data) && $user->getKey()) {
+//            $this->mixPanel->people->set($user->getKey(), $data, $this->request->ip());
+//        }
+//    }
+//
+}
--- app/Providers/MixpanelServiceProvider.php
+++ app/Providers/MixpanelServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Observers\UserObserver;
 use GeneaLabs\LaravelMixpanel\LaravelMixpanel;
 use GeneaLabs\LaravelMixpanel\Listeners\LaravelMixpanelUserObserver;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class MixpanelServiceProvider extends ServiceProvider
@@ -12,11 +14,18 @@ class MixpanelServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      *
-     * @return void
+     * @param Request $request
+     * @param LaravelMixpanel $mixPanel
      */
     public function boot(Request $request, LaravelMixpanel $mixPanel)
     {
-        $this->app->make(config('auth.model'))->observe(new LaravelMixpanelUserObserver($request, $mixPanel));
+        Log::info('BOOT MixpanelServiceProvider');
+        Log::info(config('auth.model'));
+
+//        $this->app->make(config('auth.model'))->observe(new LaravelMixpanelUserObserver($request, $mixPanel));
+
+        $this->app->make(config('auth.model'))->observe(new UserObserver($request, $mixPanel));
+
     }
 
     /**
