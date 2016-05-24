567ae1f9a5ffa6f80bf5bbecfd34378b4468e373
Dan Schultz <dan@formula23.com>
2016-05-23 17:39:52 -0700

    add migration

---
+++ database/migrations/2016_05_23_173746_change_vehicle_type_srv_attribs.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeVehicleTypeSrvAttribs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_attribs', function (Blueprint $table) {
+            \DB::statement("ALTER TABLE `service_attribs` CHANGE `vehicle_type` `vehicle_type` ENUM('Car','Truck','SUV','Van','Minivan') not null default 'Car'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_attribs', function (Blueprint $table) {
+            \DB::statement("ALTER TABLE `service_attribs` CHANGE `vehicle_type` `vehicle_type` ENUM('Car','Non-Car') not null default 'Car'");
+        });
+    }
+}
