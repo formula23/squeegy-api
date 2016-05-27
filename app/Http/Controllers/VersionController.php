f557b13d0db7cafc085639f7f525ab646e32933d
Dan Schultz <dan@formula23.com>
2016-05-26 19:06:57 -0700

    update version controllers

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -28,18 +28,14 @@ class VersionController extends Controller {
 
         if($user_app_version) {
 
-            $this->parseVersionNumber($user_app_version);
-
             $android = ($request->header('X-Device') == "Android" ? "_ANDROID" : "" );
             $app_type_key = ($user_app_type=="washer" ? "WASHER_" : "" );
 
             $install_link = env($app_type_key.'APP_INSTALL'.$android);
             $min_app_version = env($app_type_key."APP_VERSION".$android);
-
-            $this->parseVersionNumber($min_app_version, "min_app_version");
-
-            $this->requireUpgrade();
-
+            
+            $this->upgrade = version_compare($min_app_version, $user_app_version, '>');
+            
             if($this->upgrade && $install_link) {
                 return $this->response->withArray(['status'=>'upgrade', 'install_link'=>$install_link]);
             } else {
@@ -47,29 +43,4 @@ class VersionController extends Controller {
             }
         }
     }
-
-    /**
-     * @param $min_ver_number
-     * @param string $type
-     * @internal param $version_numer
-     */
-    public function parseVersionNumber($min_ver_number, $type="user_app_version")
-    {
-        preg_match("/([1-9]+)\.([0-9]+)\.*([0-9]+)*/", $min_ver_number, $req_match);
-        $this->{$type}[] = (int)$req_match[1];
-        $this->{$type}[] = (int)$req_match[2];
-        $this->{$type}[] = (int)(isset($req_match[3]) ? $req_match[3] : 0);
-
-    }
-
-    /**
-     *
-     */
-    public function requireUpgrade()
-    {
-        for($i=0;$i<=2;$i++) {
-            if($this->user_app_version[$i] < $this->min_app_version[$i]) { $this->upgrade = true; break; }
-        }
-    }
-
 }
