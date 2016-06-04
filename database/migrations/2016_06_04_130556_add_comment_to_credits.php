9b511ab81126654f5c4847010adee521d0d67f30
Dan Schultz <dan@formula23.com>
2016-06-04 13:08:33 -0700

    add description column to credits table.

---
+++ database/migrations/2016_06_04_130556_add_comment_to_credits.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCommentToCredits extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('credits', function (Blueprint $table) {
+            $table->mediumText('description')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('credits', function (Blueprint $table) {
+            $table->removeColumn('description');
+        });
+    }
+}
