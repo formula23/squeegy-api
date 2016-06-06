6da3b1cfa8093d682b8005898077f86f99a80eae
Dan Schultz <dan@formula23.com>
2016-06-06 00:09:40 -0700

    add tip eligible to order transformer

--- app/Squeegy/Transformers/OrderTransformer.php
+++ app/Squeegy/Transformers/OrderTransformer.php
@@ -45,6 +45,7 @@ class OrderTransformer extends TransformerAbstract {
             'is_subscription_order' => ($order->schedule && $order->schedule->type=='subscription'?true:false),
             'location' => $order->location,
             'instructions' => $order->instructions,
+            'tip_eligible' => true,
             'subtotal' => (int)$order->price,
             'discount' => ($order->discount ? (int)$order->discount : null ),
             'credit' => ($order->credit ? (int)$order->credit : 0 ),
