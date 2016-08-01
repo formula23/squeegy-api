584d3689b4e1c6fad19504e7c8f55ee9e431bdf8
Dan Schultz <dan@formula23.com>
2016-08-01 14:41:15 -0700

    remove logging

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -642,17 +642,17 @@ class OrdersController extends Controller {
         $incomingNumber = $request->input('From');
         $messageBody = $request->input('Body');
 
-        \Log::info('twilio: '.$twilioNumber);
-        \Log::info('incoming: '.$incomingNumber);
-        \Log::info('msg: '.$messageBody);
+//        \Log::info('twilio: '.$twilioNumber);
+//        \Log::info('incoming: '.$incomingNumber);
+//        \Log::info('msg: '.$messageBody);
 
         $order = Order::getOrderFromNumber($twilioNumber);
-        \Log::info($order);
+//        \Log::info($order);
         if( ! $order) return $this->failedSmsResponse($incomingNumber);
 
         $order_recipients = $order->getContactRecipients($incomingNumber);
 
-        \Log::info($order_recipients);
+//        \Log::info($order_recipients);
 
         if(count($order_recipients)) {
             $messageBody = $this->getSmsMessage($incomingNumber, $order, $messageBody);
