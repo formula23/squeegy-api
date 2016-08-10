63d63bd4d246ec0ab01e8d71d4b7968402bc5cc4
Dan Schultz <dan@formula23.com>
2016-08-10 15:39:45 -0700

    add King's arch partnerships.

--- database/seeds/PartnerTableSeeder.php
+++ database/seeds/PartnerTableSeeder.php
@@ -18,80 +18,30 @@ class PartnerTableSeeder extends Seeder
 //        Partner::truncate();
 
         $partner = Partner::create([
-            'name'=>'Mattel Tower',
-            'location_name'=>'Mattel Tower',
+            'name'=>"King's Arch - Wilshire",
+            'location_name'=>"King's Arch - Wilshire",
             'location'=>[
-                "city"=>"El Segundo",
-                "lat" => 33.920633,
-                "lon" => -118.391523,
-                "street"=>"333 Continental Blvd",
-                "zip"=>"90245",
+                "city"=>"Beverly Hills",
+                "lat" => 33.066798,
+                "lon" => -118.384373,
+                "street"=>"8820 Wilshire Blvd",
+                "zip"=>"90211",
                 "state"=>"CA"
             ],
-            'geo_fence'=>'[{"lat":33.921701, "lng":-118.392639},{"lat":33.921680, "lng":-118.391191},{"lat":33.920314, "lng":-118.390969},{"lat":33.920337, "lng":-118.392655}]',
+            'geo_fence'=>'[{"lat":34.067051, "lng":-118.384680},{"lat":34.067052, "lng":-118.383701},{"lat":34.066479, "lng":-118.383679},{"lat":34.066475, "lng":-118.384646}]',
             'is_active'=>1,
         ]);
 
         $partner->days()->create([
-            'day'=>'Monday',
-            'day_of_week'=>1,
-            'next_date'=>Carbon::createFromDate(2016, 8, 8),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Thursday',
+            'day'=>'Friday',
             'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 11),
+            'next_date'=>Carbon::createFromDate(2016, 8, 12),
             'time_start'=>'8:00am',
             'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
+            'frequency'=>'bi-weekly',
         ]);
 
-        foreach( [1=>1500, 2=>2500] as $service_id=>$price_override) {
-            $service = Service::find($service_id);
-            $partner->services()->save($service, ['price'=>$price_override]);
-        }
-
-
-        //////
-
-        $partner = Partner::create([
-            'name'=>'Mattel MCLC',
-            'location_name'=>'Mattel MCLC',
-            'location'=>[
-                "city"=>"El Segundo",
-                "lat" => 33.920633,
-                "lon" => -118.391523,
-                "street"=>"1955 East Grand Avenue",
-                "zip"=>"90245",
-                "state"=>"CA"
-            ],
-            'geo_fence'=>'[{"lat":33.921653, "lng":-118.393637},{"lat":33.921666, "lng":-118.392758},{"lat":33.919722, "lng":-118.392752},{"lat":33.919726, "lng":-118.393622}]',
-            'is_active'=>1,
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Monday',
-            'day_of_week'=>1,
-            'next_date'=>Carbon::createFromDate(2016, 8, 8),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Thursday',
-            'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 11),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        foreach( [1=>1500, 2=>2500] as $service_id=>$price_override) {
+        foreach( [1=>1800, 2=>2500] as $service_id=>$price_override) {
             $service = Service::find($service_id);
             $partner->services()->save($service, ['price'=>$price_override]);
         }
@@ -99,119 +49,30 @@ class PartnerTableSeeder extends Seeder
         //////
 
         $partner = Partner::create([
-            'name'=>'Mattel HTC',
-            'location_name'=>'Mattel HTC',
+            'name'=>"King's Arch - Sunset",
+            'location_name'=>"King's Arch - Sunset",
             'location'=>[
-                "city"=>"El Segundo",
-                "lat" => 33.924213,
-                "lon" => -118.391295,
-                "street"=>"2031 East Mariposa Avenue",
-                "zip"=>"90245",
+                "city"=>"Hollywood",
+                "lat" => 34.098264,
+                "lon" => -118.331619,
+                "street"=>"6515 W Sunset Blvd",
+                "zip"=>"90028",
                 "state"=>"CA"
             ],
-            'geo_fence'=>'[{"lat":33.926829, "lng":-118.391862},{"lat":33.926816, "lng":-118.390056},{"lat":33.924958, "lng":-118.390067},{"lat":33.924909, "lng":-118.389126},{"lat":33.923761, "lng":-118.389090},{"lat":33.923778, "lng":-118.391847}]',
+            'geo_fence'=>'[{"lat":34.098740, "lng":-118.331734},{"lat":34.098744, "lng":-118.331499},{"lat":34.098122, "lng":-118.331495},{"lat":34.098116, "lng":-118.331730}]',
             'is_active'=>1,
         ]);
 
         $partner->days()->create([
-            'day'=>'Monday',
-            'day_of_week'=>1,
-            'next_date'=>Carbon::createFromDate(2016, 8, 8),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Thursday',
+            'day'=>'Friday',
             'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 11),
+            'next_date'=>Carbon::createFromDate(2016, 8, 19),
             'time_start'=>'8:00am',
             'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
+            'frequency'=>'bi-weekly',
         ]);
 
-        foreach( [1=>1500, 2=>2500] as $service_id=>$price_override) {
-            $service = Service::find($service_id);
-            $partner->services()->save($service, ['price'=>$price_override]);
-        }
-
-        //////
-
-        $partner = Partner::create([
-            'name'=>'Mattel Maple',
-            'location_name'=>'Mattel Maple',
-            'location'=>[
-                "city"=>"El Segundo",
-                "lat" => 33.927259,
-                "lon" => -118.390280,
-                "street"=>"2031 East Maple Avenue",
-                "zip"=>"90245",
-                "state"=>"CA"
-            ],
-            'geo_fence'=>'[{"lat":33.927659, "lng":-118.390900},{"lat":33.927655, "lng":-118.389721},{"lat":33.927058, "lng":-118.389721},{"lat":33.927035, "lng":-118.391786},{"lat":33.927331, "lng":-118.391668},{"lat":33.927511, "lng":-118.391435},{"lat":33.927682, "lng":-118.391094}]',
-            'is_active'=>1,
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Monday',
-            'day_of_week'=>1,
-            'next_date'=>Carbon::createFromDate(2016, 8, 8),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Thursday',
-            'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 11),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        foreach( [1=>1500, 2=>2500] as $service_id=>$price_override) {
-            $service = Service::find($service_id);
-            $partner->services()->save($service, ['price'=>$price_override]);
-        }
-
-        //////
-
-        $partner = Partner::create([
-            'name'=>'Fuhu',
-            'location_name'=>'Fuhu',
-            'location'=>[
-                "city"=>"El Segundo",
-                "lat" => 33.928646,
-                "lon" => -118.397687,
-                "street"=>"1700 E Walnut Ave",
-                "zip"=>"90245",
-                "state"=>"CA"
-            ],
-            'geo_fence'=>'[{"lat":33.929005, "lng":-118.398170},{"lat":33.929004, "lng":-118.397500},{"lat":33.928135, "lng":-118.397476},{"lat":33.928122, "lng":-118.398159}]',
-            'is_active'=>1,
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Monday',
-            'day_of_week'=>1,
-            'next_date'=>Carbon::createFromDate(2016, 8, 8),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Thursday',
-            'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 11),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'weekly',
-        ]);
-
-        foreach( [1=>1500, 2=>2500] as $service_id=>$price_override) {
+        foreach( [1=>1800, 2=>2500] as $service_id=>$price_override) {
             $service = Service::find($service_id);
             $partner->services()->save($service, ['price'=>$price_override]);
         }
