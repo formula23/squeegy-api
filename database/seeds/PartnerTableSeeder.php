e96eb30acc6204fe58edabd9f93e93d90322dc1f
Dan Schultz <dan@formula23.com>
2016-08-16 13:06:37 -0700

    create manhattan towers partner location

--- database/seeds/PartnerTableSeeder.php
+++ database/seeds/PartnerTableSeeder.php
@@ -18,27 +18,27 @@ class PartnerTableSeeder extends Seeder
 //        Partner::truncate();
 
         $partner = Partner::create([
-            'name'=>"King's Arch - Wilshire",
-            'location_name'=>"King's Arch - Wilshire",
+            'name'=>"Manhattan Towers",
+            'location_name'=>"Manhattan Towers",
             'location'=>[
-                "city"=>"Beverly Hills",
-                "lat" => 33.066798,
-                "lon" => -118.384373,
-                "street"=>"8820 Wilshire Blvd",
-                "zip"=>"90211",
+                "city"=>"Manhattan Beach",
+                "lat" => 33.900856,
+                "lon" => -118.392345,
+                "street"=>"1240 Rosecrans Ave",
+                "zip"=>"90266",
                 "state"=>"CA"
             ],
-            'geo_fence'=>'[{"lat":34.067051, "lng":-118.384680},{"lat":34.067052, "lng":-118.383701},{"lat":34.066479, "lng":-118.383679},{"lat":34.066475, "lng":-118.384646}]',
+            'geo_fence'=>'[{"lat":33.901816, "lng":-118.392798},{"lat":33.901816, "lng":-118.390722},{"lat":33.900776, "lng":-118.390773},{"lat":33.900458, "lng":-118.392954}]',
             'is_active'=>1,
         ]);
 
         $partner->days()->create([
-            'day'=>'Friday',
-            'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 12),
+            'day'=>'Tuesday',
+            'day_of_week'=>2,
+            'next_date'=>Carbon::createFromDate(2016, 8, 16),
             'time_start'=>'8:00am',
             'time_end'=>'5:00pm',
-            'frequency'=>'bi-weekly',
+            'frequency'=>'weekly',
         ]);
 
         foreach( [1=>1800, 2=>2500] as $service_id=>$price_override) {
@@ -48,34 +48,34 @@ class PartnerTableSeeder extends Seeder
 
         //////
 
-        $partner = Partner::create([
-            'name'=>"King's Arch - Sunset",
-            'location_name'=>"King's Arch - Sunset",
-            'location'=>[
-                "city"=>"Hollywood",
-                "lat" => 34.098264,
-                "lon" => -118.331619,
-                "street"=>"6515 W Sunset Blvd",
-                "zip"=>"90028",
-                "state"=>"CA"
-            ],
-            'geo_fence'=>'[{"lat":34.098740, "lng":-118.331734},{"lat":34.098744, "lng":-118.331499},{"lat":34.098122, "lng":-118.331495},{"lat":34.098116, "lng":-118.331730}]',
-            'is_active'=>1,
-        ]);
-
-        $partner->days()->create([
-            'day'=>'Friday',
-            'day_of_week'=>5,
-            'next_date'=>Carbon::createFromDate(2016, 8, 19),
-            'time_start'=>'8:00am',
-            'time_end'=>'5:00pm',
-            'frequency'=>'bi-weekly',
-        ]);
-
-        foreach( [1=>1800, 2=>2500] as $service_id=>$price_override) {
-            $service = Service::find($service_id);
-            $partner->services()->save($service, ['price'=>$price_override]);
-        }
+//        $partner = Partner::create([
+//            'name'=>"King's Arch - Sunset",
+//            'location_name'=>"King's Arch - Sunset",
+//            'location'=>[
+//                "city"=>"Hollywood",
+//                "lat" => 34.098264,
+//                "lon" => -118.331619,
+//                "street"=>"6515 W Sunset Blvd",
+//                "zip"=>"90028",
+//                "state"=>"CA"
+//            ],
+//            'geo_fence'=>'[{"lat":34.098740, "lng":-118.331734},{"lat":34.098744, "lng":-118.331499},{"lat":34.098122, "lng":-118.331495},{"lat":34.098116, "lng":-118.331730}]',
+//            'is_active'=>1,
+//        ]);
+//
+//        $partner->days()->create([
+//            'day'=>'Friday',
+//            'day_of_week'=>5,
+//            'next_date'=>Carbon::createFromDate(2016, 8, 19),
+//            'time_start'=>'8:00am',
+//            'time_end'=>'5:00pm',
+//            'frequency'=>'bi-weekly',
+//        ]);
+//
+//        foreach( [1=>1800, 2=>2500] as $service_id=>$price_override) {
+//            $service = Service::find($service_id);
+//            $partner->services()->save($service, ['price'=>$price_override]);
+//        }
 
 //        DB::statement('SET FOREIGN_KEY_CHECKS = 1'); // disable foreign key constraints
 
