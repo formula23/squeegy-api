4efb0004c4887d96ed3ca9b6227ae8020698b681
Dan Schultz <dan@formula23.com>
2016-10-27 19:45:34 -0700

    update invalid message

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -84,7 +84,7 @@ return [
         ],
         'communication'=> [
             'invalid_number' => 'Thanks for calling Squeegy. This number is not valid. Goodbye!',
-            'invalid_number_sms' => "Thank you for contacting Squeegy.\n\nIf you need additional support regarding your wash, please call Squeegy Support at 424-247-8069 or email support@squeegyapp.com\nThank you!",
+            'invalid_number_sms' => "-- This is an automated message. Please do not respond. --\n\nIf you need additional support regarding your wash, please call Squeegy Support at 424-247-8069 or email support@squeegyapp.com\nThank you!",
             'sms' => [
                 'to_washer'=>":customer_name - Order# :order_id:\n:body",
                 'to_customer'=>":washer_name with Squeegy:\n:body",
